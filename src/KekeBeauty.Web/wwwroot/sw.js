6066696954909ce3823df86cfcafa0ec6f3f834d
MamadouKernel <kernel0748@gmail.com>
2026-09-26 07:34:43 +0000

    fix: restaurer la caméra et la création de boutique mobile

--- src/KekeBeauty.Web/wwwroot/sw.js
+++ src/KekeBeauty.Web/wwwroot/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'keke-beauty-v9';
+const CACHE_VERSION = 'keke-beauty-v10';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGE_CACHE = `${CACHE_VERSION}-pages`;
 const OFFLINE_URL = '/offline.html';
