228986188efaca9cb5d5cf39404e63423faa4963
MamadouKernel <kernel0748@gmail.com>
2026-09-26 07:51:33 +0000

    fix: isoler les documents KYC sélectionnés en galerie

--- src/KekeBeauty.Web/wwwroot/sw.js
+++ src/KekeBeauty.Web/wwwroot/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'keke-beauty-v10';
+const CACHE_VERSION = 'keke-beauty-v11';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGE_CACHE = `${CACHE_VERSION}-pages`;
 const OFFLINE_URL = '/offline.html';
