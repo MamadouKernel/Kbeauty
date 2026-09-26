26a8760161c4dfa5774bbe9c94fbe33ad12d6889
MamadouKernel <kernel0748@gmail.com>
2026-09-26 08:21:05 +0000

    fix: afficher les erreurs de connexion partenaire

--- src/KekeBeauty.Web/wwwroot/sw.js
+++ src/KekeBeauty.Web/wwwroot/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'keke-beauty-v12';
+const CACHE_VERSION = 'keke-beauty-v13';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGE_CACHE = `${CACHE_VERSION}-pages`;
 const OFFLINE_URL = '/offline.html';
