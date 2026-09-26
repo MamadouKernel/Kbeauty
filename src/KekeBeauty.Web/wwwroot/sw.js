f32ebaf523af03f3a71769c0783357749b5adaa5
MamadouKernel <kernel0748@gmail.com>
2026-09-26 10:55:00 +0000

    feat: détailler la validation des boutiques

--- src/KekeBeauty.Web/wwwroot/sw.js
+++ src/KekeBeauty.Web/wwwroot/sw.js
@@ -1,9 +1,9 @@
-const CACHE_VERSION = 'keke-beauty-v15';
+const CACHE_VERSION = 'keke-beauty-v16';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGE_CACHE = `${CACHE_VERSION}-pages`;
 const OFFLINE_URL = '/offline.html';
 const STATIC_ASSETS = [
-  '/', OFFLINE_URL, '/app.css?v=15', '/css/tailwind.css?v=15', '/KekeBeauty.Web.styles.css?v=15', '/manifest.webmanifest', '/img/logo.png',
+  '/', OFFLINE_URL, '/app.css?v=16', '/css/tailwind.css?v=16', '/KekeBeauty.Web.styles.css?v=16', '/manifest.webmanifest', '/img/logo.png',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/icon-512-maskable.png',
   '/icons/apple-touch-icon.png', '/fonts/material-symbols-outlined.ttf',
   '/js/keke-pwa.js', '/js/keke-push.js', '/js/keke-google.js?v=15'
